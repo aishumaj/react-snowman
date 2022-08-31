b979d06f8c380181f47e99c8e35171f8e747185a
Andrew Jensen <andrewkjensen@gmail.com>
2022-08-31 12:37:42 -0700

    add tests on Snowman

--- src/Snowman.test.js
+++ src/Snowman.test.js
@@ -14,7 +14,62 @@ test("photo displays after maximum guesses", () => {
  
 })
 
+test("correct guess displays letter and photo doesn't change", () => {
+  const { container, debug } = render(<Snowman words={["react"]} maxWrong={6} />);
+  
+  const img = container.querySelector("img");
+  expect(img.getAttribute("src")).toEqual("0.png");
+  
+  fireEvent.click(container.querySelector("#r"));
+  expect(img.getAttribute("src")).toEqual("0.png");
+  const word = container.querySelector(".Snowman-word");
+  expect(word).toContainHTML('<p class="Snowman-word">r____</p>')
+  //expect container to have last image
+  
+ 
+})
+
+test("photo and letter buttons change correctly after incorrect guess", () => {
+  const { container, debug } = render(<Snowman words={["react"]} />);
+  const img = container.querySelector("img");
+
+  expect(img.getAttribute("src")).toEqual("0.png");
+  
+  fireEvent.click(container.querySelector("#b"));
+  expect(img.getAttribute("src")).toEqual("1.png");
+  expect(container.querySelector("#b").getAttribute("disabled")).toEqual("")
+  
+  fireEvent.click(container.querySelector("#d"));
+  expect(img.getAttribute("src")).toEqual("2.png");
+  expect(container.querySelector("#d").getAttribute("disabled")).toEqual("")
+  
+  
+  fireEvent.click(container.querySelector("#f"));
+  expect(img.getAttribute("src")).toEqual("3.png");
+  expect(container.querySelector("#f").getAttribute("disabled")).toEqual("")
+  
+  
+  fireEvent.click(container.querySelector("#g"));
+  expect(img.getAttribute("src")).toEqual("4.png");
+  expect(container.querySelector("#g").getAttribute("disabled")).toEqual("")
+  
+  
+  fireEvent.click(container.querySelector("#h"));
+  expect(img.getAttribute("src")).toEqual("5.png");
+  expect(container.querySelector("#h").getAttribute("disabled")).toEqual("")
+  
+  
+  fireEvent.click(container.querySelector("#i"));
+  expect(img.getAttribute("src")).toEqual("6.png");
+  
+  //expect container to have last image
+  
+  expect(container.querySelector(".letterButtons")).not.toBeInTheDocument();
+ 
+})
+
 test("matches snapshot", function () {
   const { container } = render(<Snowman />);
   expect(container).toMatchSnapshot();
 });
+
